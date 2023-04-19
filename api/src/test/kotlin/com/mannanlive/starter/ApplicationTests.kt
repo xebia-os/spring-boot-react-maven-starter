8a6a2edf1fc62e85470f9e87aba9d4dd7c0f2597
mamackie <mamackie@expedia.com>
2023-04-19 22:45:35 +1000

    Add tests and use yaml properties.

---
+++ api/src/test/kotlin/com/mannanlive/starter/ApplicationTests.kt
@@ -0,0 +1,11 @@
+package com.mannanlive.starter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
