b255e06211b3abc9c49dfb0c9d2f8688808041ac
mamackie <mamackie@expedia.com>
2023-04-19 22:49:08 +1000

    Fix checkstyle.

--- api/src/test/kotlin/com/mannanlive/starter/ApplicationTests.kt
+++ api/src/test/kotlin/com/mannanlive/starter/ApplicationTests.kt
@@ -5,7 +5,9 @@ import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class ApplicationTests {
+
     @Test
+    @Suppress("EmptyFunctionBlock")
     fun contextLoads() {
     }
 }
